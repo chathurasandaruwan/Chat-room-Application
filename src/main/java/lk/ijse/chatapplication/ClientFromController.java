310bf12635c0c633e2031ed41462eb1a1c164e8a
chathura <chathurasandaruwan122@gmail.com>
2024-02-01 16:53:51 +0530

    send emoji done

--- src/main/java/lk/ijse/chatapplication/ClientFromController.java
+++ src/main/java/lk/ijse/chatapplication/ClientFromController.java
@@ -57,6 +57,7 @@ public class ClientFromController extends Thread{
     Socket socket;
     FileChooser fileChooser = new FileChooser();
     File selectedFile;
+    Stage primaryStage = new Stage();
     public void initialize() {
         lblUserName.setText(LoggingFromController.name);
         showMessage();
@@ -98,7 +99,7 @@ public class ClientFromController extends Thread{
     @FXML
     void emogiIconOnAction(MouseEvent event) {
         ObservableList<Emoji> emojiList = FXCollections.observableArrayList(EmojiManager.getAll());
-        Stage primaryStage = new Stage();
+//        Stage primaryStage = new Stage();
         ListView<Emoji> listView = new ListView<>(emojiList);
         listView.setCellFactory(param -> new EmojiCell());
 
@@ -110,9 +111,6 @@ public class ClientFromController extends Thread{
         primaryStage.show();
 
 
-        /*String emojiCode = ":smile:";
-        String emojiCharacter = EmojiParser.parseToUnicode(emojiCode);
-        out.println(emojiCharacter);*/
 
     }
 
@@ -142,8 +140,41 @@ public class ClientFromController extends Thread{
                 if (st.length() > 3) {
                     firstChars = st.substring(0, 3);
                 }
+      /*          if (firstChars.equalsIgnoreCase("emj")){
+                    //String emojiCode = ":smile:";
 
-                if (firstChars.equalsIgnoreCase("img")) {
+                    //String emojiCharacter = EmojiParser.parseToUnicode(emojiCode);
+                    //out.println(emojiCharacter);
+                    TextFlow tempFlow = new TextFlow();
+                   // Text emojiText = new Text(emojiCharacter + " ");
+                  //  tempFlow.getChildren().add(emojiText);
+
+                    tempFlow.setMaxWidth(200);
+
+                    TextFlow flow = new TextFlow(tempFlow);
+
+                    HBox hBox = new HBox(12);
+
+                    if (!cmd.equalsIgnoreCase(lblUserName.getText() + ":")) {
+                        messageArea.setAlignment(Pos.TOP_LEFT);
+                        hBox.setAlignment(Pos.CENTER_LEFT);
+                        hBox.setStyle("-fx-alignment: center-left;-fx-fill-height: true;-fx-min-height: 50;-fx-pref-width: 860;-fx-max-width: 860;-fx-padding: 10");
+                        flow.setStyle("-fx-background-color: white;-fx-background-radius:15;-fx-font-size: 15;-fx-font-weight: normal;-fx-text-fill: black;-fx-wrap-text: true;-fx-alignment: center-left;-fx-content-display: left;-fx-padding: 10;-fx-max-width: 350;");
+                        hBox.getChildren().add(flow);
+                    } else {
+                        Text text2 = new Text(fullMsg + ": Me");
+                        TextFlow flow2 = new TextFlow(text2);
+                        hBox.setAlignment(Pos.BOTTOM_RIGHT);
+                        hBox.setStyle("-fx-alignment: center-right;-fx-fill-height: true;-fx-min-height: 50;-fx-pref-width: 860;-fx-max-width: 860;-fx-padding: 10");
+                        flow2.setStyle("-fx-background-color:  #3dd3d0 ;-fx-background-radius:15;-fx-font-size: 15;-fx-font-weight: normal;-fx-text-fill: white;-fx-wrap-text: true;-fx-alignment: center-left;-fx-content-display: left;-fx-padding: 10;-fx-max-width: 350;");
+                        hBox.getChildren().add(flow2);
+                    }
+
+                    Platform.runLater(() -> messageArea.getChildren().addAll(hBox));
+
+
+
+                }else*/ if (firstChars.equalsIgnoreCase("img")) {
 
                     st = st.substring(3, st.length() - 1);
 
@@ -154,7 +185,7 @@ public class ClientFromController extends Thread{
                         newImageView.setFitWidth(100);
                         newImageView.setFitHeight(100);
 
-                        HBox newImageMessageBox = new HBox(10);
+                        HBox newImageMessageBox = new HBox(12);
                         newImageMessageBox.setAlignment(Pos.BOTTOM_RIGHT);
 
                     if (!cmd.equalsIgnoreCase(lblUserName.getText())) {
@@ -184,7 +215,7 @@ public class ClientFromController extends Thread{
                     }
 
                     tempFlow.getChildren().add(text);
-                    tempFlow.setMaxWidth(200);
+                    tempFlow.setMaxWidth(100);
 
                     TextFlow flow = new TextFlow(tempFlow);
 
@@ -194,7 +225,7 @@ public class ClientFromController extends Thread{
                         messageArea.setAlignment(Pos.TOP_LEFT);
                         hBox.setAlignment(Pos.CENTER_LEFT);
                         hBox.setStyle("-fx-alignment: center-left;-fx-fill-height: true;-fx-min-height: 50;-fx-pref-width: 860;-fx-max-width: 860;-fx-padding: 10");
-                        flow.setStyle("-fx-background-color:  #1988bb;-fx-background-radius:15;-fx-font-size: 15;-fx-font-weight: normal;-fx-text-fill: white;-fx-wrap-text: true;-fx-alignment: center-left;-fx-content-display: left;-fx-padding: 10;-fx-max-width: 350;");
+                        flow.setStyle("-fx-background-color: white;-fx-background-radius:15;-fx-font-size: 15;-fx-font-weight: normal;-fx-text-fill: white;-fx-wrap-text: true;-fx-alignment: center-left;-fx-content-display: left;-fx-padding: 10;-fx-max-width: 350;");
                         hBox.getChildren().add(flow);
                     } else {
                         Text text2 = new Text(fullMsg + ": Me");
@@ -218,7 +249,7 @@ public class ClientFromController extends Thread{
     void textMessageOnAction(ActionEvent event) throws IOException {
         btnSendOnAction(event);
     }
-    //for emoji
+    //for get emoji and set to text
     private class EmojiCell extends javafx.scene.control.ListCell<Emoji> {
         @Override
         protected void updateItem(Emoji item, boolean empty) {
@@ -238,6 +269,14 @@ public class ClientFromController extends Thread{
 
                 setOnMouseClicked(event -> {
                     System.out.println("Clicked Emoji Code: " + item.getAliases().get(0));
+                    String emojiCharacter = EmojiParser.parseToUnicode(":"+item.getAliases().get(0)+":");
+                    //System.out.println("text mssg "+textMessage.getText());
+                  /*  if (textMessage.getText()!=null){
+                        textMessage.setText(emojiCharacter);
+                    }else {*/
+                    textMessage.setText(textMessage.getText()+emojiCharacter);
+                        primaryStage.close();
+                  //  }
                 });
             }
         }
